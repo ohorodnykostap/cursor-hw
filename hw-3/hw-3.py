ba01a417ff6183fce1c24b0e1e811e8739303453
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2021-12-01 22:28:58 +0200

    hw3 items 1-4

--- hw-3/hw-3.py
+++ hw-3/hw-3.py
@@ -1,1 +1,49 @@
-1
+# 1. Define the id of next variables:
+int_a = 55
+str_b = 'cursor'
+set_c = {1, 2, 3}
+lst_d = [1, 2, 3]
+dict_e = {'a': 1, 'b': 2, 'c': 3}
+print(id(int_a))
+print(id(str_b))
+print(id(set_c))
+print(id(lst_d))
+print(id(dict_e))
+            # 9790720
+            # 140000319756400
+            # 140000319727424
+            # 140000320634560
+            # 140000320561024
+
+# 2. Append 4 and 5 to the lst_d and define the id one more time.
+
+lst_d.append(4)
+lst_d.append(5)
+print(id(lst_d))
+            # 140446229827264
+
+# 3. Define the type of each object from step 1.
+
+print(type(int_a))
+print(type(str_b))
+print(type(set_c))
+print(type(lst_d))
+print(type(dict_e))
+            # <class 'int'>
+            # <class 'str'>
+            # <class 'set'>
+            # <class 'list'>
+            # <class 'dict'>
+            
+# 4. Check the type of the objects by using isinstance.
+
+print(isinstance(int_a, int))
+print(isinstance(str_b, str))
+print(isinstance(set_c, set))
+print(isinstance(lst_d, list))
+print(isinstance(dict_e, dict))
+            # True
+            # True
+            # True
+            # True
+            # True
