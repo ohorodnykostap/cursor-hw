a30196dcb22d21cc322350f4f35f42ce8105b770
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2021-12-14 23:33:54 +0200

    hw-5 items 5-10

--- hw-5/hw-5.py
+++ hw-5/hw-5.py
@@ -1,3 +1,7 @@
+import dataclasses
+
+from collections import namedtuple
+
 # 1
 
 class Laptop:
@@ -64,5 +68,121 @@ class Pasta:
 
 pasta_1 = Pasta.carbonara()
 pasta_2 = Pasta.bolognaise()
-print(pasta_1)
-print(pasta_2)
+print('bolognaise', pasta_1)
+print('carbonara', pasta_2)
+
+#5
+
+class Concert:
+    max_visitors_num = 0
+
+    def __init__(self):
+        self._visitors_count = 0
+
+    @property
+    def visitors_count(self):
+        return self._visitors_count
+
+    @visitors_count.setter
+    def visitors_count(self, value):
+        if value <= self.max_visitors_num:
+            self._visitors_count = value
+        else:
+            self._visitors_count = self.max_visitors_num
+
+
+Concert.max_visitors_num = 50
+concert = Concert()
+concert.visitors_count = 1000
+print(concert.visitors_count)
+            # 50
+
+# 6
+
+
+@dataclasses.dataclass
+class AddressBookDataClass:
+    key: int
+    name: str
+    phone_number: str
+    address: str
+    email: str
+    birthday: str
+    age: int
+
+# 7
+
+
+AddressBookDataClass_1 = namedtuple('AddressBookDataClass',
+                                    ['key', 'name',
+                                     'phone_number',
+                                     'address', 'email',
+                                     'birthday', 'age'])
+
+my_data = AddressBookDataClass_1(1, 'Ostap', '82-55-878',
+                                'Lviv', 'ostap.ohorodnyk@gmail.com',
+                                '26.06', 32)
+print(my_data)
+
+#8
+
+class AddressBook:
+    def __init__(self, key, name,
+                 phone_number, address,
+                 email, birthday, age):
+        self.key = int(key)
+        self.name = str(name)
+        self.phone_number = str(phone_number)
+        self.address = str(address)
+        self.email = str(email)
+        self.birthday = str(birthday)
+        self.age = int(age)
+
+    def __str__(self):
+        return f'addbook {self.key}, {self.name},' \
+               f'{self.phone_number}, {self.address},' \
+               f'{self.email}, {self.birthday},' \
+               f'{self.age}'
+
+
+adresa = AddressBook(key=2, name='Bart',
+                     phone_number='123-45-67',
+                     address='Boston',
+                     email='Bart@gmail.com',
+                     birthday='01.01.2001',
+                     age=21)
+print(adresa)
+print(adresa.name)
+
+# 9
+
+class Person:
+    def __init__(self, name, age, country):
+        self.name = name
+        self.age = age
+        self.country = country
+
+    @property
+    def person_age(self):
+        return self.age
+
+    @person_age.setter
+    def person_age(self, value):
+        self.age = value
+
+
+john = Person('John', 36, 'USA')
+john.person_age = 32
+print(john.person_age)
+
+# 10
+
+class Student:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+student = Student(12, 'Ostap')
+setattr(student, 'email', 'Ostap@gmail.com')
+print(getattr(student, 'email'))
